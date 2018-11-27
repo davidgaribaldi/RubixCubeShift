aded19981d0c105db23015175c182b6a7c25ab94
davidgaribaldi <44825671+davidgaribaldi@users.noreply.github.com>
2018-11-26 21:47:02 -0800

    Update matrixSort.py

--- matrixSort.py
+++ matrixSort.py
@@ -5,21 +5,21 @@ Created on Mon Nov 26 11:09:46 2018
 @author: davidg
 """
 
-def rotate90(matrix, length):
-    fakeMatrix = [[] for _ in range(length)]
-    for elem in matrix:
-        fakeMatrix[0].insert(0, elem[length-length])
-        fakeMatrix[1].insert(0, elem[length-2])
-        fakeMatrix[2].insert(0, elem[length-1])
-    return fakeMatrix
-    
-        
-    
 
-new_matrix = []    
-matrix1 = [[1,2,3], ["a",'b','c'], [7,8,9]]
+def rotate90(matrix):
+    length = 0
+    length = len(matrix)
+    n = 0
+    fakeMatrix = [[] for _ in range(length)]
+    for n in range(length):
+        for elem in matrix:
+            fakeMatrix[n].insert(0, elem[n])
+    return fakeMatrix
+
+
+new_matrix = []
+matrix1 = [[1, 2, 3, 4], ["a", 'b', 'c', 'd'], [7, 8, 9, 10], ['as', 'sd', 'pl', 'ok']]
 print(matrix1)
-length = len(matrix1)
-new_matrix = rotate90(matrix1, length)
+new_matrix = rotate90(matrix1)
 
 print(new_matrix)
